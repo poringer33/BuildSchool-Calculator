4031ea7ea7c140acfd58bc994648a89558b743ab
q20274982 <q20274982@gmail.com>
2020-03-16 15:08:51 +0800

    計算機畫面

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -28,35 +28,408 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.AnswerBox = new System.Windows.Forms.TextBox();
+            this._7 = new System.Windows.Forms.Button();
+            this._8 = new System.Windows.Forms.Button();
+            this._9 = new System.Windows.Forms.Button();
+            this._6 = new System.Windows.Forms.Button();
+            this._5 = new System.Windows.Forms.Button();
+            this._4 = new System.Windows.Forms.Button();
+            this._3 = new System.Windows.Forms.Button();
+            this._2 = new System.Windows.Forms.Button();
+            this._1 = new System.Windows.Forms.Button();
+            this._Equl = new System.Windows.Forms.Button();
+            this.Dot = new System.Windows.Forms.Button();
+            this._0 = new System.Windows.Forms.Button();
+            this.Percent = new System.Windows.Forms.Button();
+            this.LBrackets = new System.Windows.Forms.Button();
+            this.RBrackets = new System.Windows.Forms.Button();
+            this._x = new System.Windows.Forms.Button();
+            this.Deg = new System.Windows.Forms.Button();
+            this.Rad = new System.Windows.Forms.Button();
+            this.Power = new System.Windows.Forms.Button();
+            this.Exp = new System.Windows.Forms.Button();
+            this.Ans = new System.Windows.Forms.Button();
+            this.Root = new System.Windows.Forms.Button();
+            this.Tan = new System.Windows.Forms.Button();
+            this.E = new System.Windows.Forms.Button();
+            this.Log = new System.Windows.Forms.Button();
+            this.Cos = new System.Windows.Forms.Button();
+            this.Pi = new System.Windows.Forms.Button();
+            this.ln = new System.Windows.Forms.Button();
+            this.sin = new System.Windows.Forms.Button();
+            this.Lnv = new System.Windows.Forms.Button();
+            this.AC = new System.Windows.Forms.Button();
+            this.Add = new System.Windows.Forms.Button();
+            this.Less = new System.Windows.Forms.Button();
+            this.Multiply = new System.Windows.Forms.Button();
+            this.except = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // AnswerBox
             // 
-            this.button1.Location = new System.Drawing.Point(363, 133);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.AnswerBox.Location = new System.Drawing.Point(27, 54);
+            this.AnswerBox.Multiline = true;
+            this.AnswerBox.Name = "AnswerBox";
+            this.AnswerBox.Size = new System.Drawing.Size(686, 65);
+            this.AnswerBox.TabIndex = 0;
             // 
-            // label1
+            // _7
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(213, 65);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 15);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "計算機";
+            this._7.Location = new System.Drawing.Point(317, 189);
+            this._7.Name = "_7";
+            this._7.Size = new System.Drawing.Size(94, 36);
+            this._7.TabIndex = 1;
+            this._7.Text = "7";
+            this._7.UseVisualStyleBackColor = true;
+            // 
+            // _8
+            // 
+            this._8.Location = new System.Drawing.Point(398, 189);
+            this._8.Name = "_8";
+            this._8.Size = new System.Drawing.Size(94, 36);
+            this._8.TabIndex = 2;
+            this._8.Text = "8";
+            this._8.UseVisualStyleBackColor = true;
+            // 
+            // _9
+            // 
+            this._9.Location = new System.Drawing.Point(479, 189);
+            this._9.Name = "_9";
+            this._9.Size = new System.Drawing.Size(94, 36);
+            this._9.TabIndex = 3;
+            this._9.Text = "9";
+            this._9.UseVisualStyleBackColor = true;
+            // 
+            // _6
+            // 
+            this._6.Location = new System.Drawing.Point(479, 239);
+            this._6.Name = "_6";
+            this._6.Size = new System.Drawing.Size(94, 36);
+            this._6.TabIndex = 6;
+            this._6.Text = "6";
+            this._6.UseVisualStyleBackColor = true;
+            // 
+            // _5
+            // 
+            this._5.Location = new System.Drawing.Point(398, 239);
+            this._5.Name = "_5";
+            this._5.Size = new System.Drawing.Size(94, 36);
+            this._5.TabIndex = 5;
+            this._5.Text = "5";
+            this._5.UseVisualStyleBackColor = true;
+            // 
+            // _4
+            // 
+            this._4.Location = new System.Drawing.Point(317, 239);
+            this._4.Name = "_4";
+            this._4.Size = new System.Drawing.Size(94, 36);
+            this._4.TabIndex = 4;
+            this._4.Text = "4";
+            this._4.UseVisualStyleBackColor = true;
+            // 
+            // _3
+            // 
+            this._3.Location = new System.Drawing.Point(479, 293);
+            this._3.Name = "_3";
+            this._3.Size = new System.Drawing.Size(94, 36);
+            this._3.TabIndex = 9;
+            this._3.Text = "3";
+            this._3.UseVisualStyleBackColor = true;
+            // 
+            // _2
+            // 
+            this._2.Location = new System.Drawing.Point(398, 293);
+            this._2.Name = "_2";
+            this._2.Size = new System.Drawing.Size(94, 36);
+            this._2.TabIndex = 8;
+            this._2.Text = "2";
+            this._2.UseVisualStyleBackColor = true;
+            // 
+            // _1
+            // 
+            this._1.Location = new System.Drawing.Point(317, 293);
+            this._1.Name = "_1";
+            this._1.Size = new System.Drawing.Size(94, 36);
+            this._1.TabIndex = 7;
+            this._1.Text = "1";
+            this._1.UseVisualStyleBackColor = true;
+            // 
+            // _Equl
+            // 
+            this._Equl.Location = new System.Drawing.Point(479, 347);
+            this._Equl.Name = "_Equl";
+            this._Equl.Size = new System.Drawing.Size(94, 36);
+            this._Equl.TabIndex = 12;
+            this._Equl.Text = "=";
+            this._Equl.UseVisualStyleBackColor = true;
+            // 
+            // Dot
+            // 
+            this.Dot.Location = new System.Drawing.Point(398, 347);
+            this.Dot.Name = "Dot";
+            this.Dot.Size = new System.Drawing.Size(94, 36);
+            this.Dot.TabIndex = 11;
+            this.Dot.Text = ".";
+            this.Dot.UseVisualStyleBackColor = true;
+            // 
+            // _0
+            // 
+            this._0.Location = new System.Drawing.Point(317, 347);
+            this._0.Name = "_0";
+            this._0.Size = new System.Drawing.Size(94, 36);
+            this._0.TabIndex = 10;
+            this._0.Text = "0";
+            this._0.UseVisualStyleBackColor = true;
+            // 
+            // Percent
+            // 
+            this.Percent.Location = new System.Drawing.Point(479, 135);
+            this.Percent.Name = "Percent";
+            this.Percent.Size = new System.Drawing.Size(94, 36);
+            this.Percent.TabIndex = 15;
+            this.Percent.Text = "%";
+            this.Percent.UseVisualStyleBackColor = true;
+            // 
+            // LBrackets
+            // 
+            this.LBrackets.Location = new System.Drawing.Point(398, 135);
+            this.LBrackets.Name = "LBrackets";
+            this.LBrackets.Size = new System.Drawing.Size(94, 36);
+            this.LBrackets.TabIndex = 14;
+            this.LBrackets.Text = ")";
+            this.LBrackets.UseVisualStyleBackColor = true;
+            // 
+            // RBrackets
+            // 
+            this.RBrackets.Location = new System.Drawing.Point(317, 135);
+            this.RBrackets.Name = "RBrackets";
+            this.RBrackets.Size = new System.Drawing.Size(94, 36);
+            this.RBrackets.TabIndex = 13;
+            this.RBrackets.Text = "(";
+            this.RBrackets.UseVisualStyleBackColor = true;
+            // 
+            // _x
+            // 
+            this._x.Location = new System.Drawing.Point(189, 135);
+            this._x.Name = "_x";
+            this._x.Size = new System.Drawing.Size(94, 36);
+            this._x.TabIndex = 30;
+            this._x.Text = "x!";
+            this._x.UseVisualStyleBackColor = true;
+            // 
+            // Deg
+            // 
+            this.Deg.Location = new System.Drawing.Point(108, 135);
+            this.Deg.Name = "Deg";
+            this.Deg.Size = new System.Drawing.Size(94, 36);
+            this.Deg.TabIndex = 29;
+            this.Deg.Text = "Deg";
+            this.Deg.UseVisualStyleBackColor = true;
+            // 
+            // Rad
+            // 
+            this.Rad.Location = new System.Drawing.Point(27, 135);
+            this.Rad.Name = "Rad";
+            this.Rad.Size = new System.Drawing.Size(94, 36);
+            this.Rad.TabIndex = 28;
+            this.Rad.Text = "Rad";
+            this.Rad.UseVisualStyleBackColor = true;
+            // 
+            // Power
+            // 
+            this.Power.Location = new System.Drawing.Point(189, 347);
+            this.Power.Name = "Power";
+            this.Power.Size = new System.Drawing.Size(94, 36);
+            this.Power.TabIndex = 27;
+            this.Power.Text = "Xy";
+            this.Power.UseVisualStyleBackColor = true;
+            // 
+            // Exp
+            // 
+            this.Exp.Location = new System.Drawing.Point(108, 347);
+            this.Exp.Name = "Exp";
+            this.Exp.Size = new System.Drawing.Size(94, 36);
+            this.Exp.TabIndex = 26;
+            this.Exp.Text = "EXP";
+            this.Exp.UseVisualStyleBackColor = true;
+            // 
+            // Ans
+            // 
+            this.Ans.Location = new System.Drawing.Point(27, 347);
+            this.Ans.Name = "Ans";
+            this.Ans.Size = new System.Drawing.Size(94, 36);
+            this.Ans.TabIndex = 25;
+            this.Ans.Text = "Ans";
+            this.Ans.UseVisualStyleBackColor = true;
+            // 
+            // Root
+            // 
+            this.Root.Location = new System.Drawing.Point(189, 293);
+            this.Root.Name = "Root";
+            this.Root.Size = new System.Drawing.Size(94, 36);
+            this.Root.TabIndex = 24;
+            this.Root.Text = "√";
+            this.Root.UseVisualStyleBackColor = true;
+            // 
+            // Tan
+            // 
+            this.Tan.Location = new System.Drawing.Point(108, 293);
+            this.Tan.Name = "Tan";
+            this.Tan.Size = new System.Drawing.Size(94, 36);
+            this.Tan.TabIndex = 23;
+            this.Tan.Text = "tan";
+            this.Tan.UseVisualStyleBackColor = true;
+            // 
+            // E
+            // 
+            this.E.Location = new System.Drawing.Point(27, 293);
+            this.E.Name = "E";
+            this.E.Size = new System.Drawing.Size(94, 36);
+            this.E.TabIndex = 22;
+            this.E.Text = "e";
+            this.E.UseVisualStyleBackColor = true;
+            // 
+            // Log
+            // 
+            this.Log.Location = new System.Drawing.Point(189, 239);
+            this.Log.Name = "Log";
+            this.Log.Size = new System.Drawing.Size(94, 36);
+            this.Log.TabIndex = 21;
+            this.Log.Text = "log";
+            this.Log.UseVisualStyleBackColor = true;
+            // 
+            // Cos
+            // 
+            this.Cos.Location = new System.Drawing.Point(108, 239);
+            this.Cos.Name = "Cos";
+            this.Cos.Size = new System.Drawing.Size(94, 36);
+            this.Cos.TabIndex = 20;
+            this.Cos.Text = "cos";
+            this.Cos.UseVisualStyleBackColor = true;
+            // 
+            // Pi
+            // 
+            this.Pi.Location = new System.Drawing.Point(27, 239);
+            this.Pi.Name = "Pi";
+            this.Pi.Size = new System.Drawing.Size(94, 36);
+            this.Pi.TabIndex = 19;
+            this.Pi.Text = " π";
+            this.Pi.UseVisualStyleBackColor = true;
+            // 
+            // ln
+            // 
+            this.ln.Location = new System.Drawing.Point(189, 189);
+            this.ln.Name = "ln";
+            this.ln.Size = new System.Drawing.Size(94, 36);
+            this.ln.TabIndex = 18;
+            this.ln.Text = "ln";
+            this.ln.UseVisualStyleBackColor = true;
+            // 
+            // sin
+            // 
+            this.sin.Location = new System.Drawing.Point(108, 189);
+            this.sin.Name = "sin";
+            this.sin.Size = new System.Drawing.Size(94, 36);
+            this.sin.TabIndex = 17;
+            this.sin.Text = "sin";
+            this.sin.UseVisualStyleBackColor = true;
+            // 
+            // Lnv
+            // 
+            this.Lnv.Location = new System.Drawing.Point(27, 189);
+            this.Lnv.Name = "Lnv";
+            this.Lnv.Size = new System.Drawing.Size(94, 36);
+            this.Lnv.TabIndex = 16;
+            this.Lnv.Text = "lnv";
+            this.Lnv.UseVisualStyleBackColor = true;
+            // 
+            // AC
+            // 
+            this.AC.Location = new System.Drawing.Point(619, 135);
+            this.AC.Name = "AC";
+            this.AC.Size = new System.Drawing.Size(94, 36);
+            this.AC.TabIndex = 35;
+            this.AC.Text = "AC";
+            this.AC.UseVisualStyleBackColor = true;
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(619, 347);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(94, 36);
+            this.Add.TabIndex = 34;
+            this.Add.Text = "+";
+            this.Add.UseVisualStyleBackColor = true;
+            // 
+            // Less
+            // 
+            this.Less.Location = new System.Drawing.Point(619, 293);
+            this.Less.Name = "Less";
+            this.Less.Size = new System.Drawing.Size(94, 36);
+            this.Less.TabIndex = 33;
+            this.Less.Text = "-";
+            this.Less.UseVisualStyleBackColor = true;
+            // 
+            // Multiply
+            // 
+            this.Multiply.Location = new System.Drawing.Point(619, 239);
+            this.Multiply.Name = "Multiply";
+            this.Multiply.Size = new System.Drawing.Size(94, 36);
+            this.Multiply.TabIndex = 32;
+            this.Multiply.Text = "*";
+            this.Multiply.UseVisualStyleBackColor = true;
+            // 
+            // except
+            // 
+            this.except.Location = new System.Drawing.Point(619, 189);
+            this.except.Name = "except";
+            this.except.Size = new System.Drawing.Size(94, 36);
+            this.except.TabIndex = 31;
+            this.except.Text = "/";
+            this.except.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.AC);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.Less);
+            this.Controls.Add(this.Multiply);
+            this.Controls.Add(this.except);
+            this.Controls.Add(this._x);
+            this.Controls.Add(this.Deg);
+            this.Controls.Add(this.Rad);
+            this.Controls.Add(this.Power);
+            this.Controls.Add(this.Exp);
+            this.Controls.Add(this.Ans);
+            this.Controls.Add(this.Root);
+            this.Controls.Add(this.Tan);
+            this.Controls.Add(this.E);
+            this.Controls.Add(this.Log);
+            this.Controls.Add(this.Cos);
+            this.Controls.Add(this.Pi);
+            this.Controls.Add(this.ln);
+            this.Controls.Add(this.sin);
+            this.Controls.Add(this.Lnv);
+            this.Controls.Add(this.Percent);
+            this.Controls.Add(this.LBrackets);
+            this.Controls.Add(this.RBrackets);
+            this.Controls.Add(this._Equl);
+            this.Controls.Add(this.Dot);
+            this.Controls.Add(this._0);
+            this.Controls.Add(this._3);
+            this.Controls.Add(this._2);
+            this.Controls.Add(this._1);
+            this.Controls.Add(this._6);
+            this.Controls.Add(this._5);
+            this.Controls.Add(this._4);
+            this.Controls.Add(this._9);
+            this.Controls.Add(this._8);
+            this.Controls.Add(this._7);
+            this.Controls.Add(this.AnswerBox);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -66,8 +439,42 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox AnswerBox;
+        private System.Windows.Forms.Button _7;
+        private System.Windows.Forms.Button _8;
+        private System.Windows.Forms.Button _9;
+        private System.Windows.Forms.Button _6;
+        private System.Windows.Forms.Button _5;
+        private System.Windows.Forms.Button _4;
+        private System.Windows.Forms.Button _3;
+        private System.Windows.Forms.Button _2;
+        private System.Windows.Forms.Button _1;
+        private System.Windows.Forms.Button _Equl;
+        private System.Windows.Forms.Button Dot;
+        private System.Windows.Forms.Button _0;
+        private System.Windows.Forms.Button Percent;
+        private System.Windows.Forms.Button LBrackets;
+        private System.Windows.Forms.Button RBrackets;
+        private System.Windows.Forms.Button _x;
+        private System.Windows.Forms.Button Deg;
+        private System.Windows.Forms.Button Rad;
+        private System.Windows.Forms.Button Power;
+        private System.Windows.Forms.Button Exp;
+        private System.Windows.Forms.Button Ans;
+        private System.Windows.Forms.Button Root;
+        private System.Windows.Forms.Button Tan;
+        private System.Windows.Forms.Button E;
+        private System.Windows.Forms.Button Log;
+        private System.Windows.Forms.Button Cos;
+        private System.Windows.Forms.Button Pi;
+        private System.Windows.Forms.Button ln;
+        private System.Windows.Forms.Button sin;
+        private System.Windows.Forms.Button Lnv;
+        private System.Windows.Forms.Button AC;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Less;
+        private System.Windows.Forms.Button Multiply;
+        private System.Windows.Forms.Button except;
     }
 }
 
--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -16,5 +16,7 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+
     }
 }
